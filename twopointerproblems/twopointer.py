55781f1a2b89dd95b6c1cdac599266743717d3f3
vigneshthatchana <70791244+vigneshthatchana@users.noreply.github.com>
2026-03-17 19:09:34 +0530

    Update twopointer.py

--- twopointerproblems/twopointer.py
+++ twopointerproblems/twopointer.py
@@ -1,1 +1,8 @@
+def twopointer(arr,target):
+  left = 0
+  right = len(arr)-1
+  while left < right:
+    current_sum = arr[left] + arr[right]
 
+    if current_sum == target:
+      return left,right
