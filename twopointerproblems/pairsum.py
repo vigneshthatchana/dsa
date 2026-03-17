113fec64bd55094a684bb5ba23f5f7bde97bbb35
vigneshthatchana <70791244+vigneshthatchana@users.noreply.github.com>
2026-03-17 19:30:34 +0530

    Update pairsum.py

--- twopointerproblems/pairsum.py
+++ twopointerproblems/pairsum.py
@@ -1,1 +1,17 @@
-def 
+def pairsum(arr,target):
+  left = 0
+  right = len(arr)-1
+  count = 0
+
+  while left < right:
+    current_sum = arr[left] + arr[right]
+    if current_sum == target:
+      count += 1
+      left += 1
+      right -= 1
+    elif current_sum < target:
+      left += 1
+    else:
+      right -= 1
+  return None
+
