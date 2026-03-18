23a8d496bbce28b926e126120d5e88faa7d44eb8
vigneshthatchana <70791244+vigneshthatchana@users.noreply.github.com>
2026-03-18 22:09:29 +0530

    Update binarysearch.py

--- binarysearchproblems/binarysearch.py
+++ binarysearchproblems/binarysearch.py
@@ -1,1 +1,14 @@
+def binarysearch(arr,target):
+  left = 0
+  right = len(arr)-1
 
+  while left < right:
+    mid = (left+right) // 2
+
+    if arr[mid] == target:
+      return mid 
+    elif arr[mid] < target:
+      left += 1
+    else:
+      right -= 1
+  return -1
