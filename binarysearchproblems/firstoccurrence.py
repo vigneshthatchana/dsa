0df72b681adaa8aa5de8ec535869abb18bbb78aa
vigneshthatchana <70791244+vigneshthatchana@users.noreply.github.com>
2026-03-18 22:21:03 +0530

    Update firstoccurrence.py

--- binarysearchproblems/firstoccurrence.py
+++ binarysearchproblems/firstoccurrence.py
@@ -1,1 +1,13 @@
+def first_occur(arr,target):
+  left = 0
+  right = len(arr)-1
+  ans = -1
 
+  while left <= right:
+    mid = (left+right) // 2
+
+    if arr[mid] == target:
+      ans = mid
+      right = mid -1
+    elif arr[mid] < target:
+      left = mid + 1
