24206897c58f70d77b20d426fc2b58105844a331
vigneshthatchana <70791244+vigneshthatchana@users.noreply.github.com>
2026-03-28 23:48:28 +0530

    Update pivotindex.py

--- prefixsum/pivotindex.py
+++ prefixsum/pivotindex.py
@@ -3,3 +3,9 @@ def pivotindex(arr):
   left_sum = 0
 
   for i in range(len(arr)):
+       right_sum = total_sum - left_sum-arr[i]
+       if left_sum == right_sum:
+         return i
+
+        left_sum += arr[i]
+    return -1
